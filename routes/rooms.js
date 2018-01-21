e2fc7bc837d445dd36f0674424b04524fc4625a7
Webster Bei Yijie <bei.yijie.2014@vjc.sg>
2018-01-21 02:18:02 -0500

    updated removal, addition of TA, modification of room information, list students in room

--- routes/rooms.js
+++ routes/rooms.js
@@ -91,7 +91,9 @@ router.delete('/:roomid', function(req, res, next) {
 });
 
 
-/* */
+/*
+  This function allows the retrieval of detailed information of a room
+*/
 router.get('/:roomid', function(req, res, next) {
   var roomid = req.params.roomid;
   Room.findOne({_id: roomid}).lean().exec(function(err, room) {
@@ -107,12 +109,109 @@ router.get('/:roomid', function(req, res, next) {
           res.status(500);
           res.send('Redis Error: Unable to retrieve some information from redis');
         } else {
-          res.status(200);
-          if(!status || status=='inactive') res.send('inactive');
-          else res.send('active');
+          room.status = status ? status : 'inactive';
+          redis.smembers('room:'+roomid+':students', function(err, students) {
+            if(err) {
+              res.status(500);
+              res.send('Redis Error: Unable to retrieve some information from redis');
+            } else {
+              room.students_in_room = students ? students : [];
+              res.status(200);
+              res.send(room);
+            }
+          });
         }
       });
-      //res.send(room);
+    }
+  });
+});
+
+/*
+  This functions allows the modification of basic room information
+*/
+router.put('/:roomid', function(req, res, next) {
+  var roomid = req.params.roomid;
+  var professor = req.body.professor;
+  var course = req.body.course;
+  var update = {};
+  if(professor) update.professor = professor;
+  if(course) update.course = course;
+  Room.update({_id: roomid}, {$set: update}, function (err, opResult) {
+    if(err) {
+      res.status(500);
+      res.send('MongoDB Error: Unable to update room information');
+    } else {
+      res.status(200);
+      var time = new Date();
+      res.send({roomid: roomid, modified_at: time.toISOString()});
+    }
+  });
+});
+
+/*
+  This functions allows the retrieval of a list of TAs in the room
+*/
+router.get('/:roomid/TAs', function(req, res, next) {
+  var roomid = req.params.roomid;
+  Room.findOne({_id: roomid}, function(err, room) {
+    if(err) {
+      res.status(500);
+      res.send('MongoDB Error: Unable to retrieve TA list from mongodb');
+    } else {
+      res.status(200);
+      res.send({TA_list: room.TA_list});
+    }
+  })
+});
+
+/*
+  This function allows the addition of a TA to a room
+*/
+router.put('/:roomid/TAs/:netid', function(req, res, next) {
+  var roomid = req.params.roomid;
+  var netid = req.params.netid;
+  Room.findByIdAndUpdate(roomid, {$addToSet: {TA_list: netid}}, {new: true}, function(err, room) {
+    if(err) {
+      res.status(500);
+      res.send('MongoDB Error: Unable to add TA to the room');
+    } else {
+      res.status(200);
+      var time = new Date();
+      res.send({roomid: roomid, TA_list: room.TA_list, updated_at: time.toISOString()});
+    }
+  });
+});
+
+/*
+  This function allows the removal of a TA from a room
+*/
+router.delete('/:roomid/TAs/:netid', function(req, res, next) {
+  var roomid = req.params.roomid;
+  var netid = req.params.netid;
+  Room.update({_id: roomid}, {$pull: {TA_list: netid}}, function(err, opResult) {
+    if(err) {
+      res.status(500);
+      res.send('MongoDB Error: Unable to remove TA from room');
+    } else {
+      res.status(204);
+      res.send();
+    }
+  });
+});
+
+/*
+  This function allows the retrieval of a list of students in a room
+*/
+router.get('/:roomid/students', function(req, res, next) {
+  var roomid = req.params.roomid;
+  redis.smembers('room:'+roomid+':students', function(err, students) {
+    if(err) {
+      res.status(500);
+      res.send('Redis Error: Unable to retrieve the list of students');
+    } else {
+      res.status(200);
+      console.log(students);
+      res.send({students_in_room: students ? students : []});
     }
   });
 });
